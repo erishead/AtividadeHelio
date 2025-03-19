b7733a704e041ca4a538f48d91df789d5cc242d1
erishead <erisvanaraujo1910@gmail.com>
2025-03-19 17:50:49 -0300

    ex12.java

--- ex12.java
+++ ex12.java
@@ -1,15 +1,18 @@
 import javax.swing.JOptionPane;
 
-public class Exercicio13 {
+public class Exercicio12 {
     public static void main(String[] args) {
-        StringBuilder resultado = new StringBuilder("Números que atendem a condição:\n");
+        int maior = Integer.MIN_VALUE, menor = Integer.MAX_VALUE;
+        double soma = 0;
 
-        for (int i = 1000; i <= 1999; i++) {
-            if (i % 11 == 5) {
-                resultado.append(i).append("\n");
-            }
+        for (int i = 0; i < 500; i++) {
+            int num = Integer.parseInt(JOptionPane.showInputDialog("Digite um número positivo:"));
+            if (num > maior) maior = num;
+            if (num < menor) menor = num;
+            soma += num;
         }
 
-        JOptionPane.showMessageDialog(null, resultado.toString());
+        double media = soma / 500;
+        JOptionPane.showMessageDialog(null, "Maior: " + maior + "\nMenor: " + menor + "\nMédia: " + media);
     }
 }
