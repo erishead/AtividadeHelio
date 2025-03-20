3acc693c4a06fba264220ca584fd8fa19e69e6b5
erishead <erisvanaraujo1910@gmail.com>
2025-03-19 21:26:18 -0300

    Atualizar o atividade2 ex27.java

--- atividade2 ex27.java
+++ atividade2 ex27.java
@@ -1,12 +1,44 @@
 import javax.swing.JOptionPane;
 
-public class Ex28 {
+public class Ex27 {
     public static void main(String[] args) {
-        int n = Integer.parseInt(JOptionPane.showInputDialog("Digite um número:"));
-        if (n == (30 + 25) * (30 + 25)) {
-            JOptionPane.showMessageDialog(null, "O número possui essa característica!");
-        } else {
-            JOptionPane.showMessageDialog(null, "O número NÃO possui essa característica.");
-        }
+        // Entrada de dados
+        String nome1 = JOptionPane.showInputDialog("Digite o nome da 1ª pessoa:");
+        int idade1 = Integer.parseInt(JOptionPane.showInputDialog("Digite a idade da 1ª pessoa:"));
+        double altura1 = Double.parseDouble(JOptionPane.showInputDialog("Digite a altura da 1ª pessoa (m):"));
+
+        String nome2 = JOptionPane.showInputDialog("Digite o nome da 2ª pessoa:");
+        int idade2 = Integer.parseInt(JOptionPane.showInputDialog("Digite a idade da 2ª pessoa:"));
+        double altura2 = Double.parseDouble(JOptionPane.showInputDialog("Digite a altura da 2ª pessoa (m):"));
+
+        String nome3 = JOptionPane.showInputDialog("Digite o nome da 3ª pessoa:");
+        int idade3 = Integer.parseInt(JOptionPane.showInputDialog("Digite a idade da 3ª pessoa:"));
+        double altura3 = Double.parseDouble(JOptionPane.showInputDialog("Digite a altura da 3ª pessoa (m):"));
+
+        // Quem é o mais alto
+        String maisAlto = (altura1 > altura2 && altura1 > altura3) ? nome1 :
+                          (altura2 > altura1 && altura2 > altura3) ? nome2 : nome3;
+
+        // Quem é o mais baixo
+        String maisBaixo = (altura1 < altura2 && altura1 < altura3) ? nome1 :
+                           (altura2 < altura1 && altura2 < altura3) ? nome2 : nome3;
+
+        // Altura média do grupo
+        double alturaMedia = (altura1 + altura2 + altura3) / 3;
+
+        // Quem é o mais velho
+        String maisVelho = (idade1 > idade2 && idade1 > idade3) ? nome1 :
+                           (idade2 > idade1 && idade2 > idade3) ? nome2 : nome3;
+
+        // Quem é o mais novo
+        String maisNovo = (idade1 < idade2 && idade1 < idade3) ? nome1 :
+                          (idade2 < idade1 && idade2 < idade3) ? nome2 : nome3;
+
+        // Exibição dos resultados
+        JOptionPane.showMessageDialog(null, "Mais alto: " + maisAlto +
+                                            "\nMais baixo: " + maisBaixo +
+                                            "\nAltura média do grupo: " + alturaMedia +
+                                            "\nMais velho: " + maisVelho +
+                                            "\nMais novo: " + maisNovo);
     }
 }
